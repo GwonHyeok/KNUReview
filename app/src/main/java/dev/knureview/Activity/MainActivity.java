11390fc7b9c0f1c5ffe673deba2b4d4f1a83f049
HA DONGHYUN <dhha22@naver.com>
2015-11-22 12:52:11 +0900

    complete of the navigation drawer

--- app/src/main/java/dev/knureview/Activity/MainActivity.java
+++ app/src/main/java/dev/knureview/Activity/MainActivity.java
@@ -1,9 +1,16 @@
 package dev.knureview.Activity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
@@ -13,12 +20,23 @@ import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import dev.knureview.Fragment.PageFragment;
 import dev.knureview.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //toogle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        toogle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(toogle);
 
 
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
@@ -32,5 +50,26 @@ public class MainActivity extends AppCompatActivity {
 
         SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
         viewPagerTab.setViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toogle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toogle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toogle.onConfigurationChanged(newConfig);
     }
 }
